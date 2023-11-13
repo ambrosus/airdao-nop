1bcc8c9b7e0f0257b9f77841494fbdb27341c578
svin <svinerus@gmail.com>
2023-11-13 16:48:46 +0200

    Fix check_status

--- src/phases/06_check_status_in_server_nodes.ts
+++ src/phases/06_check_status_in_server_nodes.ts
@@ -9,28 +9,27 @@ This Source Code Form is “Incompatible With Secondary Licenses”, as defined 
 import {
   Contracts,
   Methods,
-  ContractNames,
-} from "@airdao/airdao-node-contracts";
-import Dialog from "../dialogs/dialog_model";
-import { ethers } from "ethers";
-import { Network } from "../interfaces/network";
-import Crypto from "../utils/crypto";
+  ContractNames
+} from '@airdao/airdao-node-contracts';
+import Dialog from '../dialogs/dialog_model';
+import {ethers} from 'ethers';
+import {Network} from '../interfaces/network';
+import Crypto from '../utils/crypto';
 
 export default async function checkStatusInServerNodes(
   privateKey: string,
   network: Network
 ) {
-  const currentTimeInSeconds = Date.now() / 1000;
   const provider = new ethers.providers.JsonRpcProvider(network.rpc);
 
   const signer = new ethers.VoidSigner(ethers.constants.AddressZero, provider);
-  const { chainId } = await provider.getNetwork();
+  const {chainId} = await provider.getNetwork();
 
   const address = Crypto.addressForPrivateKey(privateKey);
 
   const contracts = new Contracts(signer, chainId);
   const validator = await Methods.getApolloInfo(contracts, address);
-  if (!validator) {
+  if (validator.apollo.stake.isZero()) {
     Dialog.notRegisteredDialog(network.domain);
     return;
   }
@@ -38,21 +37,19 @@ export default async function checkStatusInServerNodes(
   if (validator.isOnboarded) {
     Dialog.alreadyOnboardedDialog(network.domain, address);
     return;
-  } else if (!validator.isOnboarded) {
-    const contract = contracts.getContractByName(
-      ContractNames.ServerNodesManager
-    );
-    const onboardingDelay = (await contract.onboardingDelay()).toNumber();
-    const timeToWait =
-      onboardingDelay -
-      (currentTimeInSeconds - validator.apollo.timestampStake.toNumber());
-
-    const days = Math.floor(timeToWait / (3600 * 24));
-    const hours = Math.floor((timeToWait % (3600 * 24)) / 3600);
-    const minutes = Math.floor((timeToWait % 3600) / 60);
-    Dialog.waitOnboardingDialog(days, hours, minutes);
-    return;
   }
 
-  return true;
+  const contract = contracts.getContractByName(
+    ContractNames.ServerNodesManager
+  );
+  const onboardingDelay = (await contract.onboardingDelay()).toNumber();
+  const currentTimeInSeconds = Date.now() / 1000;
+  const timeToWait =
+    onboardingDelay -
+    (currentTimeInSeconds - validator.apollo.timestampStake.toNumber());
+
+  const days = Math.floor(timeToWait / (3600 * 24));
+  const hours = Math.floor((timeToWait % (3600 * 24)) / 3600);
+  const minutes = Math.floor((timeToWait % 3600) / 60);
+  Dialog.waitOnboardingDialog(days, hours, minutes);
 }
