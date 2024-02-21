f6dac1816303a8fc2b354f781915dda506cea0e3
DiMaAB <43337815+DiMaAB@users.noreply.github.com>
2024-02-21 22:34:15 +0200

    Revert "Fix `update.sh` path in `fixVersionIssue` func"

--- src/menu_actions/fix_issues_action.ts
+++ src/menu_actions/fix_issues_action.ts
@@ -77,7 +77,7 @@ async function checkFork(providerLocal: ethers.providers.JsonRpcProvider, provid
 
 async function fixVersionIssue() {
   console.log('The NOP will be updated now and node will be restarted...\nYou can run NOP again after the update is complete.');
-  await execCmd('./update.sh');
+  await execCmd('./update.sh', { cwd: '../../' });
   process.exit(0);
 }
 
